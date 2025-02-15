1c672c5cbce3a7eee3a7d2834d232fb81f82b590
Alexander Pleshakov <alexanderpleshakovone@gmail.com>
2025-02-15 10:21:12 +0300

    Initial Commit

---
+++ PredatorAndPrey/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PredatorAndPrey
+//
+//  Created by Александр Плешаков on 15.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PredatorAndPrey/PredatorAndPreyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredatorAndPreyApp.swift
+//  PredatorAndPrey
+//
+//  Created by Александр Плешаков on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct PredatorAndPreyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
